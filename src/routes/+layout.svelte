a30e08b3ec3b898860c3e6a0b7c69a21bd8f13b6
msnvk <133189118+msnvk@users.noreply.github.com>
2023-05-25 09:10:57 +0400

    Add files via upload

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,46 @@
+<nav>
+    <div class="nav-container">
+    <a href="/" class="nav-logo" title="Back to Homepage">Ульяна Корепанова</a>
+    <div class="nav-links">
+        {#each nav as link}
+        {#if link.isShow}
+        <a href={link.href} class="link">{link.title}</a>
+        {/if}        {/each}
+    </div>
+    </div>
+    </nav>
+    <div class="container">
+    <!-- Pages will be injected below -->
+    <slot></slot>
+    </div>
+    <style>
+    .container {
+    max-width: 1400px;
+    margin: 50px auto;
+    }
+    </style>
+
+<script>
+    const nav = [
+    {
+    title: 'Обо мне',
+    href: '/about',
+    isShow: true
+    },
+    {
+    title: 'Блог',
+    href: '/blog',
+    isShow: true
+    },
+    {
+    title: 'Портфолио',
+    href: '/project',
+    isShow: true
+    },
+    {
+    title: 'Контакты',
+    href: '/contact',
+    isShow: true
+    }
+    ]
+</script>
